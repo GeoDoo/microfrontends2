48d111e026c82fe8ad2bec6b9dd41960fd13bdff
Georgios Karametas <geometas@hotmail.com>
2021-12-25 19:32:44 +0000

    add progress bar

--- container/src/App.js
+++ container/src/App.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
-import Marketing from "./components/Marketing";
-import Auth from "./components/Auth";
 import Header from "./components/Header";
+import Progress from "./components/Progress";
+
+const Marketing = lazy(() => import("./components/Marketing"));
+const Auth = lazy(() => import("./components/Auth"));
 
 export default () => {
   return (
     <BrowserRouter>
       <div>
         <Header />
-        <Switch>
-          <Route path="/auth" component={Auth} />
-          <Route path="/" component={Marketing} />
-        </Switch>
+        <Suspense fallback={<Progress />}>
+          <Switch>
+            <Route path="/auth" component={Auth} />
+            <Route path="/" component={Marketing} />
+          </Switch>
+        </Suspense>
       </div>
     </BrowserRouter>
   );
